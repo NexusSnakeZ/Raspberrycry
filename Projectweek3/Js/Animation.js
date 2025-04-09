7d752f6795498fb3ddb4040dc605bda78033dd45
NexusSnakeZ <101937@glr.nl>
2025-04-09 12:30:44 +0000

    Alot of header problems

---
+++ Projectweek3/Js/Animation.js
@@ -0,0 +1,7 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("mobile-menu").classList.toggle("visible");
+});
+
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("hamburger").classList.toggle("active");
+});
